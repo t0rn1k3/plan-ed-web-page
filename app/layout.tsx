2e047cbdb75570600461114df15e75dd90af47bf
t0rn1k3 <tornikeburjanadze2@gmail.com>
2025-11-11 09:57:21 +0400

    first steps of adding elements logo and background image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../styles/globals.css";
+import "../styles/fonts.css";
+// import "alk-life/css/alk-life.min.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
