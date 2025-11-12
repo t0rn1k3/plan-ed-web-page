31994982e5c7b56a41044357e5736f15c6be2430
t0rn1k3 <tornikeburjanadze2@gmail.com>
2025-11-12 15:03:06 +0400

    create logo and add on page

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,16 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <header className="main-h-screen bg-black text-gray-900">
-        <div className="flex items-center  w-20 h-20">
-          <Image src="/logo.png" alt="logo" width={80} height={80} />
+      <header className="header text-gray-900">
+        <div className="flex items-center  h-30">
+          <Image src="/logo.png" alt="logo" width={250} height={50} />
         </div>
       </header>
       <main>
         <div className="w-7xl m-auto mt-15">
-          <h1 className="text-8xl text-red-700 ">PlanEd</h1>
+          <h1 className="text-8xl">
+            <span>P</span>lan<span>E</span>d
+          </h1>
         </div>
       </main>
     </>
